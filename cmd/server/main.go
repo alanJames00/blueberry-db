e6a08d759290c7f2dd4e34e13563ff443a6722a9
alanjames00 <alanjames6282@gmail.com>
2024-10-22 09:31:47 +0530

    add: goroutine based concurrency

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,25 +13,25 @@ import (
 
 func main() {
 	// load the configuration
-	cfg := config.LoadConfig();
+	cfg := config.LoadConfig()
 
 	// setup logging
-	logger.InitLogger(cfg.LogLevel);
+	logger.InitLogger(cfg.LogLevel)
 
 	// setup aof
-	aof, err := velocitydb.NewAof(cfg.AofFilePath);
+	aof, err := velocitydb.NewAof(cfg.AofFilePath)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error loading aof file: %s", err.Error()));
-		return;
+		logger.Error(fmt.Sprintf("Error loading aof file: %s", err.Error()))
+		return
 	}
-	defer aof.Close();
+	defer aof.Close()
 
 	// reload previous commands from aof file
-	logger.Info(fmt.Sprintf("restoring previous database state from: %s", cfg.AofFilePath));
+	logger.Info(fmt.Sprintf("restoring previous database state from: %s", cfg.AofFilePath))
 
 	aof.Read(func(value velocitydb.Value) {
-		command := strings.ToUpper(value.GetArray()[0].GetBulk());
-		args := value.GetArray()[1:];
+		command := strings.ToUpper(value.GetArray()[0].GetBulk())
+		args := value.GetArray()[1:]
 
 		handler, ok := velocitydb.Handlers[command]
 		if !ok {
@@ -39,70 +39,78 @@ func main() {
 			return
 		}
 
-		handler(args);
+		handler(args)
 	})
 
-	logger.Info(fmt.Sprintf("previous database state restored successfully"));
-	
+	logger.Info(fmt.Sprintf("previous database state restored successfully"))
+
 	// listen on the port
-	ln, err := net.Listen("tcp", cfg.ServerPort);
+	ln, err := net.Listen("tcp", cfg.ServerPort)
 	if err != nil {
 		logger.Error("error starting server. err: " + err.Error())
-		os.Exit(1);
+		os.Exit(1)
 	}
-	defer ln.Close();
+	defer ln.Close()
 
-	logger.Info("velocitydb started on port" + cfg.ServerPort);
+	logger.Info("velocitydb started on port" + cfg.ServerPort)
 
+	// accept and handle connections in loop
 
-	// accept and handle connections
-	conn, err := ln.Accept();
-	if err != nil {
-		logger.Error("error accepting clients. err: " + err.Error());
-	}
-	defer conn.Close();
-
-	// handler loop
 	for {
-
-		resp := velocitydb.NewResp(conn);
-		value, err := resp.Read();
+		conn, err := ln.Accept()
 		if err != nil {
-			logger.Error(fmt.Sprintf("error reading command: %s", err.Error()));
-			return;
-		}
-		
-		if value.GetType() != "array" {
-			logger.Error("Invalid Request, expected array");
-			continue;
-		} 
-		
-		if len(value.GetArray()) == 0 {
-			logger.Error("Invalid Request, expected array length > 0");
-			continue;
+			logger.Error("error accepting clients. err: " + err.Error())
+			continue
 		}
 
-		command := strings.ToUpper(value.GetArray()[0].GetBulk());
-		args := value.GetArray()[1:];
+		go handleConnection(conn, aof);
+
+	}
+
+}
+
+// goroutine to handle individual connection
+func handleConnection(conn net.Conn, aof *velocitydb.Aof) {
+	defer conn.Close()
+
+	for {
+		resp := velocitydb.NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			logger.Error(fmt.Sprintf("error reading command: %s", err.Error()))
+			return
+		}
+
+		if value.GetType() != "array" {
+			logger.Error("Invalid Request, expected array")
+			continue
+		}
+
+		if len(value.GetArray()) == 0 {
+			logger.Error("Invalid Request, expected array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.GetArray()[0].GetBulk())
+		args := value.GetArray()[1:]
 
 		// debug command
 
-		writer := velocitydb.NewWriter(conn);
+		writer := velocitydb.NewWriter(conn)
 
-		handler, ok := velocitydb.Handlers[command];
+		handler, ok := velocitydb.Handlers[command]
 		if !ok {
-			logger.Error("Invalid Command: " + command);
-			writer.Write(*velocitydb.NewValue("string", "", 0, "", nil));
-			continue;
+			logger.Error("Invalid Command: " + command)
+			writer.Write(*velocitydb.NewValue("string", "", 0, "", nil))
+			continue
 		}
 
 		// write to aof for set and hset commands
 		if command == "SET" || command == "HSET" {
-			aof.Write(value);
+			aof.Write(value)
 		}
 
-		result := handler(args);
-		writer.Write(result);
+		result := handler(args)
+		writer.Write(result)
 	}
-
 }
